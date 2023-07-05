74397eedb6ffc899a487ddc1b7ba17103cfb24be
Edson Garcia <ednosmab@yahoo.com.br>
2023-07-05 18:14:54 -0300

    criando rota aos métodos index e view do controller Pets

--- config/routes.php
+++ config/routes.php
@@ -45,17 +45,16 @@ return static function (RouteBuilder $routes) {
     $routes->setRouteClass(DashedRoute::class);
 
     $routes->scope('/', function (RouteBuilder $builder) {
-        /*
-         * Here, we are connecting '/' (base path) to a controller called 'Pages',
-         * its action called 'display', and we pass a param to select the view file
-         * to use (in this case, templates/Pages/home.php)...
-         */
-        $builder->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
+        
+        $builder->connect('/', ['controller' => 'Pets', 'action' => 'index']);
 
-        /*
-         * ...and connect the rest of 'Pages' controller's URLs.
-         */
-        $builder->connect('/pages/*', 'Pages::display');
+        $builder->connect('/pet/:id', [
+            ['controller' => 'Pets', 'action' => 'view'],
+            [
+                'pass' => ['id'],
+                'id' => '[0-9]+'
+            ]
+        ]);
 
         /*
          * Connect catchall routes for all controllers.
