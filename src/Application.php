7d1ae43e68e7df9df6b057ab1736970c79bf37e1
Edson Garcia <ednosmab@yahoo.com.br>
2023-07-05 18:50:15 -0300

    Criando um middleware para exemplificar a sua utilização

--- src/Application.php
+++ src/Application.php
@@ -16,6 +16,7 @@ declare(strict_types=1);
  */
 namespace App;
 
+use App\Middleware\AdPETCookieMiddleware;
 use Cake\Core\Configure;
 use Cake\Core\ContainerInterface;
 use Cake\Datasource\FactoryLocator;
@@ -101,7 +102,9 @@ class Application extends BaseApplication
             // https://book.cakephp.org/4/en/security/csrf.html#cross-site-request-forgery-csrf-middleware
             ->add(new CsrfProtectionMiddleware([
                 'httponly' => true,
-            ]));
+            ]))
+            
+            ->add(new AdPETCookieMiddleware());
 
         return $middlewareQueue;
     }
