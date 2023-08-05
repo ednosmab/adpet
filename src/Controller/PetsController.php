e533d9f8b5f4a599248745efce87ac3f84efadbb
Edson Garcia <ednosmab@yahoo.com.br>
2023-08-05 11:04:45 -0300

    Refazendo controller Pets manualmente para adquirir entendimento sobre os métodos

--- src/Controller/PetsController.php
+++ src/Controller/PetsController.php
@@ -19,13 +19,7 @@ class PetsController extends AppController
      */
     public function index()
     {
-        // $this->render('/Pets/index');
-        // $this->set('message', 'Aprendendo CakePHP');
-        $dados = [
-            'message' => 'Aprendendo CakePHP',
-            'version' => '4.0'
-        ];
-        $this->set($dados);
+
     }
 
     /**
@@ -37,84 +31,7 @@ class PetsController extends AppController
      */
     public function view($id = null)
     {
-        try {
-            $pet = $this->Pets->get($id, [
-                'contain' => []
-            ]);
 
-        } catch (RecordNotFoundException $ex) {
-            return $this->redirect(['action' => 'index'], 404);
-            
-        } finally{
-            if(!isset($pet)){
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->set('pet', $pet);
-            $this->set('_serialize', ['pet']);
-        }
     }
 
-    /**
-     * Add method
-     *
-     * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
-     */
-    public function add()
-    {
-        $pet = $this->Pets->newEmptyEntity();
-        if ($this->request->is('post')) {
-            $pet = $this->Pets->patchEntity($pet, $this->request->getData());
-            if ($this->Pets->save($pet)) {
-                $this->Flash->success(__('The pet has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The pet could not be saved. Please, try again.'));
-        }
-        $this->set(compact('pet'));
-    }
-
-    /**
-     * Edit method
-     *
-     * @param string|null $id Pet id.
-     * @return \Cake\Http\Response|null|void Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function edit($id = null)
-    {
-        $pet = $this->Pets->get($id, [
-            'contain' => [],
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $pet = $this->Pets->patchEntity($pet, $this->request->getData());
-            if ($this->Pets->save($pet)) {
-                $this->Flash->success(__('The pet has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The pet could not be saved. Please, try again.'));
-        }
-        $this->set(compact('pet'));
-    }
-
-    /**
-     * Delete method
-     *
-     * @param string|null $id Pet id.
-     * @return \Cake\Http\Response|null|void Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $pet = $this->Pets->get($id);
-        if ($this->Pets->delete($pet)) {
-            $this->Flash->success(__('The pet has been deleted.'));
-        } else {
-            $this->Flash->error(__('The pet could not be deleted. Please, try again.'));
-        }
-
-        return $this->redirect(['action' => 'index']);
-    }
 }
